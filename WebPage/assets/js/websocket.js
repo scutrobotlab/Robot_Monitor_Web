fef22903e463dd71e9d98065dc5b9f95dc2e8620
M3chD09 <m3chd09@protonmail.com>
2020-05-02 19:24:03 +0800

    ws提示修改

--- WebPage/assets/js/websocket.js
+++ WebPage/assets/js/websocket.js
@@ -6,14 +6,13 @@ window.addEventListener("load", function(evt) {
         }
         ws = new WebSocket("ws://"+document.domain+":8080/ws");
         ws.onopen = function(evt) {
-            alert("Websocket connection established");
+            toastShow("连接成功",0);
         }
         ws.onclose = function(evt) {
             ws = null;
         }
         ws.onmessage = function(evt) {
             var jsonWS = JSON.parse(evt.data);
-            console.log(jsonWS.DataPack[0].Data);
             var l = chartData.length;
             var d = {'x':l,'y':jsonWS.DataPack[0].Data};
             chartData.push(d);
