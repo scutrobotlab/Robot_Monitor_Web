ebbc5bf908452e2277bdfe2608467dfb4c9f643b
M3chD09 <m3chd09@protonmail.com>
2020-05-02 23:36:07 +0800

    test build

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+cp -r WebPage WebPageBuild
+a=$(awk -F "\""  '/http(.+?)/{print $2}' WebPageBuild/index.html)
+wget $a
+sed -i 's/https[a-zA-Z0-9\:\/\.@\-]*\///g' WebPageBuild/index.html
+go get github.com/go-bindata/go-bindata/...
+go get github.com/elazarl/go-bindata-assetfs/...
+go-bindata -fs -prefix "WebPageBuild/" WebPageBuild/
+go-bindata-assetfs WebPageBuild/...
+go build
+#rm -rf WebPageBuild
