3ea02ccbe791637746f0e58db1357d3c6402ba77
M3chD09 <m3chd09@protonmail.com>
2020-05-02 23:42:46 +0800

    fix dir

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cp -r WebPage WebPageBuild
 a=$(awk -F "\""  '/http(.+?)/{print $2}' WebPageBuild/index.html)
-wget $a
+wget -q $a -P WebPageBuild
 sed -i 's/https[a-zA-Z0-9\:\/\.@\-]*\///g' WebPageBuild/index.html
 go get github.com/go-bindata/go-bindata/...
 go get github.com/elazarl/go-bindata-assetfs/...
