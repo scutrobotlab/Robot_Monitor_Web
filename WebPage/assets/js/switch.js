a29450ce83bc82505659287fef762b6ef77ef2e0
M3chD09 <m3chd09@protonmail.com>
2020-05-08 20:13:40 +0800

    前端配置保存

---
+++ WebPage/assets/js/switch.js
@@ -0,0 +1,42 @@
+axios.get('/file/config')
+    .then(function (response) {
+        $("[name='checkbox-sda']").bootstrapSwitch('state',response.data.IsSaveDataAddr,true)
+        $("[name='checkbox-svm']").bootstrapSwitch('state',response.data.IsSaveVariablesToMod,true)
+        $("[name='checkbox-svr']").bootstrapSwitch('state',response.data.IsSaveVariablesToRead,true)
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+
+$.fn.bootstrapSwitch.defaults.onText = '是';
+$.fn.bootstrapSwitch.defaults.offText = '否';
+$.fn.bootstrapSwitch.defaults.onColor = 'info';
+$.fn.bootstrapSwitch.defaults.offColor = 'danger';
+$.fn.bootstrapSwitch.defaults.size = 'mini';
+$("[name='checkbox-sda']").bootstrapSwitch({
+    onSwitchChange:function(event,state){
+        axios.get('/file/config', {
+            params: {
+                sda: state
+            }
+        })
+    }
+});
+$("[name='checkbox-svr']").bootstrapSwitch({
+    onSwitchChange:function(event,state){
+        axios.get('/file/config', {
+            params: {
+                svr: state
+            }
+        })
+    }
+});
+$("[name='checkbox-svm']").bootstrapSwitch({
+    onSwitchChange:function(event,state){
+        axios.get('/file/config', {
+            params: {
+                svm: state
+            }
+        })
+    }
+});
