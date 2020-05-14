467fc01b0860c79b7bfdafced8be5858eab02139
M3chD09 <m3chd09@protonmail.com>
2020-05-14 16:23:52 +0800

    图表显示更新

--- WebPage/assets/js/chart.js
+++ WebPage/assets/js/chart.js
@@ -1,5 +1,8 @@
 const el = document.getElementById('chart');
 const chart = new TimeChart(el, {
-    series: []
+    baseTime: Date.now() - performance.now(),
+    series: [],
+    xRange: { min: 0, max: 20 * 1000 },
+    realTime: true
 });
-const colors=new Array('black','red','yellow','blue','green');
+const colors=new Array('#F44336','#9C27B0','#3F51B5','#00BCD4','#4CAF50','#FF9800','#795548');
