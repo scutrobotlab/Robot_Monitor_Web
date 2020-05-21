fa09a1510e7a56e7d4a494882a788f8364fc30dd
M3chD09 <m3chd09@protonmail.com>
2020-05-22 07:35:24 +0800

    fix build

--- build.sh
+++ build.sh
@@ -9,11 +9,11 @@ sed -i 's/http.Dir(\".\/frontend\/dist\/\")/assetFS()/g' main.go
 #sed -i 's/https[a-zA-Z0-9\:\/\.@\-]*\///g' WebPageBuild/index.html
 go get github.com/go-bindata/go-bindata/...
 go get github.com/elazarl/go-bindata-assetfs/...
-go-bindata -fs -prefix "frontend/dist/" frontend/dist/
-go-bindata-assetfs frontend/dist/...
+go-bindata-assetfs -prefix frontend frontend/dist/...
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o Robot_Monitor_Web_linux
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags="-w -s" -o Robot_Monitor_Web_mac
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags="-w -s" -o Robot_Monitor_Web_windows.exe
+upx -q -9 Robot_Monitor_Web_linux Robot_Monitor_Web_mac Robot_Monitor_Web_windows.exe
 zip Robot_Monitor_Web_linux.zip Robot_Monitor_Web_linux
 zip Robot_Monitor_Web_mac.zip Robot_Monitor_Web_mac
 zip Robot_Monitor_Web_windows.zip Robot_Monitor_Web_windows.exe
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.14
 require (
 	github.com/creack/goselect v0.1.1 // indirect
 	github.com/elazarl/go-bindata-assetfs v1.0.0
+	github.com/go-bindata/go-bindata v3.1.2+incompatible // indirect
 	github.com/gorilla/websocket v1.4.2
 	go.bug.st/serial.v1 v0.0.0-20191202182710-24a6610f0541
 	golang.org/x/sys v0.0.0-20200519105757-fe76b779f299 // indirect
