96d41d95be0622d79177ea83a88e6a67135dd938
M3chD09 <m3chd09@protonmail.com>
2020-05-21 22:23:08 +0800

    添加变量对话框优化

--- frontend/src/components/VariableNewDialog.vue
+++ frontend/src/components/VariableNewDialog.vue
@@ -2,36 +2,50 @@
   <v-row justify="center">
     <v-dialog v-model="dialog" persistent max-width="600px">
       <v-card>
-        <v-card-title>
-          <span class="headline">添加变量</span>
-        </v-card-title>
+        <v-toolbar dense color="primary">
+          <v-toolbar-title>添加变量</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-btn icon dark @click="dialog = false">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-toolbar>
         <v-card-text>
           <v-container>
-            <v-row>
-              <v-col cols="12">
-                <v-text-field label="板子代号" type="number" hint="保持默认为1即可" required v-model="Board"></v-text-field>
-              </v-col>
-              <v-col cols="12">
-                <v-text-field label="变量名" type="text" required v-model="Name"></v-text-field>
-              </v-col>
-              <v-col cols="12">
-                <v-select
-                  :items="types"
-                  label="变量类型"
-                  required
-                  v-model="Type"
-                ></v-select>
-              </v-col>
-              <v-col cols="12">
-                <v-text-field label="变量地址" type="text" hint="形如2000ab78" required v-model="Addr"></v-text-field>
-              </v-col>
-            </v-row>
+            <v-form ref="form" v-model="valid" lazy-validation>
+              <v-row>
+                <v-col cols="12" sm="6" md="6">
+                  <v-select
+                    :items=[1,2,3]
+                    label="板子代号"
+                    hint="保持默认为1即可"
+                    :rules="[v => !!v || '板子代号是必要的']"
+                    required
+                    v-model="Board"
+                  ></v-select>
+                </v-col>
+                <v-col cols="12" sm="6" md="6">
+                  <v-select
+                    :items="types"
+                    label="变量类型"
+                    :rules="[v => !!v || '变量类型是必要的']"
+                    required
+                    v-model="Type"
+                  ></v-select>
+                </v-col>
+                 <v-col cols="12" sm="6" md="6">
+                  <v-text-field label="变量名" type="text" :rules="[v => !!v || '变量名是必要的']" required v-model="Name"></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="6">
+                  <v-text-field label="变量地址" type="text" :rules="AddrRules" hint="形如2000ab78" required v-model="Addr"></v-text-field>
+                </v-col>
+              </v-row>
+            </v-form>
           </v-container>
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" text @click="dialog = false">关闭</v-btn>
-          <v-btn color="blue darken-1" text @click="addVariable()">添加</v-btn>
+          <v-btn color="error" @click="$refs.form.reset()">清空</v-btn>
+          <v-btn color="primary" :disabled="!valid" @click="addVariable()">添加</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -51,11 +65,16 @@
     ],
     data: () => ({
       dialog: false,
+      valid: true,
       types: [],
       Board: 1,
       Name: '',
       Type: '',
       Addr: '',
+      AddrRules: [
+        v => !!v || '变量地址是必要的',
+        v => /2[0-9a-f]{7}/.test(v) || '格式错误，应形如2000ab78',
+      ]
     }),
     mounted(){
       axios.get('/variable/types')
@@ -68,22 +87,24 @@
         this.dialog = true
       },
       addVariable(){
-        axios.post('/variable-'+this.opt+'/add', {
-            Board: 1,
-            Name: this.Name,
-            Type: this.Type,
-            Addr: parseInt(this.Addr,16),
-        })
-        .then(response =>{
-          if (response.data.status==0){
-            this.dialog = false
-            this.$refs.notice.show('变量添加成功',0)
-          }else if (response.data.status==22){
-            this.$refs.notice.show('变量操作时串口错误',1)
-          }else if (response.data.status==23){
-            this.$refs.notice.show('重复添加变量',1)
-          }
-        })
+        if (this.$refs.form.validate()){
+          axios.post('/variable-'+this.opt+'/add', {
+              Board: 1,
+              Name: this.Name,
+              Type: this.Type,
+              Addr: parseInt(this.Addr,16),
+          })
+          .then(response =>{
+            if (response.data.status==0){
+              this.dialog = false
+              this.$refs.notice.show('变量添加成功',0)
+            }else if (response.data.status==22){
+              this.$refs.notice.show('变量操作时串口错误',1)
+            }else if (response.data.status==23){
+              this.$refs.notice.show('重复添加变量',1)
+            }
+          })
+        }
       },
     }
   }
