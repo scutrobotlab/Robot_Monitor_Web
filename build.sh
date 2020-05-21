f178e79f2bfe06571b47abb82dbe6eac70d6eb73
M3chD09 <m3chd09@protonmail.com>
2020-05-21 22:44:32 +0800

    test new build

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
-cp -r WebPage WebPageBuild
+cd ./frontend
+npm install
+npm run build
+cd ../backend
+cp -r ../frontend/dist ./WebPageBuild
 cp main.go main.go.org
 sed -i 's/http.Dir(\".\/WebPage\/\")/assetFS()/g' main.go
-wget -q $(awk -F "\""  '/http(.+?)/{print $2}' WebPageBuild/index.html) -P WebPageBuild
-sed -i 's/https[a-zA-Z0-9\:\/\.@\-]*\///g' WebPageBuild/index.html
+#wget -q $(awk -F "\""  '/http(.+?)/{print $2}' WebPageBuild/index.html) -P WebPageBuild
+#sed -i 's/https[a-zA-Z0-9\:\/\.@\-]*\///g' WebPageBuild/index.html
 go get github.com/go-bindata/go-bindata/...
 go get github.com/elazarl/go-bindata-assetfs/...
 go-bindata -fs -prefix "WebPageBuild/" WebPageBuild/
