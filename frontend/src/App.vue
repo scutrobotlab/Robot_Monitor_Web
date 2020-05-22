cc82ba37dbe95d1b210c442b6fb31e7a94f4cea9
M3chD09 <m3chd09@protonmail.com>
2020-05-22 23:19:45 +0800

    关于对话框

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,8 +5,17 @@
     <v-app-bar app dark color="primary">
       <v-app-bar-nav-icon @click.stop="switchDrawer()"></v-app-bar-nav-icon>
       <v-toolbar-title>坠好用的上位机</v-toolbar-title>
+      <v-btn icon v-on:click="openDialog()">
+        <v-icon>mdi-information-outline</v-icon>
+      </v-btn>
       <v-spacer></v-spacer>
-      <v-switch v-model="$vuetify.theme.dark" hide-details inset color="black" label="深色模式"></v-switch>
+      <v-switch
+        v-model="$vuetify.theme.dark"
+        hide-details
+        inset
+        color="black"
+        v-bind:label="$vuetify.theme.dark?'深色模式':'浅色模式'"
+      ></v-switch>
     </v-app-bar>
 
     <v-content>
@@ -16,14 +25,18 @@
     <v-footer app color="primary">
       <span class="white--text">华工机器人实验室</span>
     </v-footer>
+
+    <AboutDialog ref="AboutDialog" />
   </v-app>
 </template>
 
 <script>
 import DrawerList from "@/components/DrawerList.vue";
+import AboutDialog from "@/components/AboutDialog.vue";
 export default {
   components: {
-    DrawerList
+    DrawerList,
+    AboutDialog
   },
   props: {
     source: String
@@ -31,6 +44,9 @@ export default {
   methods: {
     switchDrawer() {
       this.$refs.DrawerList.switchDrawer();
+    },
+    openDialog() {
+      this.$refs.AboutDialog.openDialog();
     }
   }
 };
